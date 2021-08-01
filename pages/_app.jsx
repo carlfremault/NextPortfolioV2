95938caa2c828e81997a75a73dd02c87752ab25f
Carl Fremault <carlfremault@yahoo.com>
2021-08-01 17:12:32 +0200

    navbar implemented

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../theme/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
---
+++ theme/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#0896A6",
+    },
+    secondary: {
+      main: "#0889A6",
+    },
+    background: {
+      default: "#8AA3A6",
+    },
+  },
+});
+
+export default theme;
