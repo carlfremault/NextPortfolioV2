d40db708637c571e9050e31131e776a4ee3b0cbf
Carl Fremault <carlfremault@yahoo.com>
2021-08-15 15:13:19 +0200

    added profilebuttons

--- components/apropos/Bio.jsx
+++ components/apropos/Bio.jsx
@@ -1,11 +1,31 @@
-import { Container, Typography } from "@material-ui/core";
+import {
+  Button,
+  Container,
+  Grid,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-const bio = () => {
+const useStyles = makeStyles((theme) => ({
+  button: {
+    borderColor: theme.palette.text.primary,
+    fontFamily: "Roboto",
+    fontWeight: 400,
+    textTransform: "none",
+    top: "1.5rem",
+  },
+}));
+
+const Bio = () => {
+  const classes = useStyles();
+
   return (
     <Container
       style={{
         marginTop: "4rem",
-        marginBottom: "4rem",
+        marginBottom: "6rem",
       }}
     >
       <Typography
@@ -65,8 +85,42 @@ const bio = () => {
           m&apos;entraîne sur Exercism et Codewars.
         </Typography>
       </Container>
+      <Container maxWidth="sm">
+        <Grid container justifyContent="space-around">
+          <Grid item>
+            <a
+              href="https://www.linkedin.com/in/carl-fremault/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button
+                variant="outlined"
+                className={classes.button}
+                startIcon={<LinkedInIcon />}
+              >
+                LinkedIn
+              </Button>
+            </a>
+          </Grid>
+          <Grid item>
+            <a
+              href="https://github.com/carlfremault"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Button
+                variant="outlined"
+                className={classes.button}
+                startIcon={<GitHubIcon />}
+              >
+                GitHub
+              </Button>
+            </a>
+          </Grid>
+        </Grid>
+      </Container>
     </Container>
   );
 };
 
-export default bio;
+export default Bio;
