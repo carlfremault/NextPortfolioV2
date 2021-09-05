0266fc5a5b7bffe6e67de10bfc2226a30c59164c
Carl Fremault <carlfremault@yahoo.com>
2021-09-05 16:32:10 +0200

    implementation of CompetenceCard component

---
+++ components/apropos/CompetenceCard.jsx
@@ -0,0 +1,47 @@
+import PropTypes from "prop-types";
+import { useTheme } from "@material-ui/styles";
+import {
+  Card,
+  CardContent,
+  Grid,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
+import CheckIcon from "@material-ui/icons/Check";
+
+/**
+ * Card for Competences component
+ * @param {string} title - Card header
+ * @param {array} items - Array of strings for list entries
+ */
+const CompetenceCard = ({ title, items }) => {
+  const theme = useTheme();
+  return (
+    <Grid item component={Card} style={theme.competenceCard} raised={false}>
+      <CardContent>
+        <Typography variant="h5" color="secondary" align="center">
+          {title}
+        </Typography>
+        <List>
+          {items.map((item) => (
+            <ListItem key={item}>
+              <ListItemIcon style={{ transform: "translate(10px)" }}>
+                <CheckIcon color="secondary" />
+              </ListItemIcon>
+              <ListItemText>{item}</ListItemText>
+            </ListItem>
+          ))}
+        </List>
+      </CardContent>
+    </Grid>
+  );
+};
+
+CompetenceCard.PropTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+};
+export default CompetenceCard;
