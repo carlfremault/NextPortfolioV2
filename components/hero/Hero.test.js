02102afd47bc6a07dc1006e06b164e33da2fda1a
Carl Fremault <carlfremault@yahoo.com>
2021-09-15 13:27:41 +0200

    modified hero test after context insertion

--- components/hero/Hero.test.js
+++ components/hero/Hero.test.js
@@ -1,16 +1,33 @@
-import { cleanup, screen } from "@testing-library/react";
-import RenderWithTheme from "../../utils/RenderWithTheme";
+import { cleanup, render, screen } from "@testing-library/react";
+import { ColorContext } from "../../pages/_app";
+import { ThemeProvider } from "@material-ui/styles";
+import darkTheme from "../../theme/darkTheme";
 import Hero from "./Hero";
 
 afterEach(cleanup);
 
+const toggleDarkMode = jest.fn();
+let darkMode = true;
+
 describe("<Hero />", () => {
   it("Render without crashing", () => {
-    RenderWithTheme(<Hero />);
+    render(
+      <ColorContext.Provider value={{ darkMode, toggleDarkMode }}>
+        <ThemeProvider theme={darkTheme}>
+          <Hero />
+        </ThemeProvider>
+      </ColorContext.Provider>
+    );
   });
 
   it("Shows hero content in DOM", () => {
-    RenderWithTheme(<Hero />);
+    render(
+      <ColorContext.Provider value={{ darkMode, toggleDarkMode }}>
+        <ThemeProvider theme={darkTheme}>
+          <Hero />
+        </ThemeProvider>
+      </ColorContext.Provider>
+    );
     expect(screen.getByText(/carl fremault/i)).toBeInTheDocument;
     expect(screen.getByText(/étudiant développeur/i)).toBeInTheDocument;
     expect(screen.getByText(/découvrez mes projets/i)).toBeInTheDocument;
